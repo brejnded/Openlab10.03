3d80c4e3c353bcb031d0b412258ccc0eead74461
Matusko <matusnekoranec22@gmail.com>
2022-12-05 18:50:24 +0100

    Add project files.

---
+++ Openlab10.03/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_10._03
+{
+    internal class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+
+        public Book()
+        {
+
+        }
+        public string Title { get { return title; } set { title = value; } }
+
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                pages = pages = value <= 0 ? 1 : value;
+            }
+        }
+        public string Category { get { return category; } set { category = value; } }
+        public string Author { get { return author; } set { author = value; } }
+
+        public int ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+
+            }
+            set
+            {
+                releaseDate = releaseDate = value <= 1450 || value >= 2021 ? -1 : value;
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format($"{title}\n{pages}\n{category}\n{author}\n{releaseDate}");
+        }
+    }
+}
---
+++ Openlab10.03/Program.cs
@@ -0,0 +1,23 @@
+﻿using Open_Lab_10._03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_10._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR.Title = "Lord Of The Rings";
+            LOTR.Pages = 576;
+            LOTR.Category = "Fantasy";
+            LOTR.Author = "J.R.R.Tolkien";
+            LOTR.ReleaseDate = 1954;
+            Console.WriteLine(LOTR.ToString());
+        }
+    }
+}
